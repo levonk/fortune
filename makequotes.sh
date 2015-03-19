e2d327495c84cb8748849d02a8b5ded87c0d7877
LevonK <git@lk.oib.com>
2015-03-19 09:03:11 -0700

    Might as well add the package installation

--- makequotes.sh
+++ makequotes.sh
@@ -2,5 +2,8 @@
 
 INFILE=levonkquotes
 DESTDIR=/usr/share/games/fortunes
-sudo strfile -c % ${INFILE} ${DESTDIR}/${INFILE}.dat && sudo cp ${INFILE} ${DESTDIR}/.
+sudo aptitude install -y fortune-mod && \
+	sudo strfile -c % ${INFILE} ${DESTDIR}/${INFILE}.dat && \
+	sudo cp ${INFILE} ${DESTDIR}/.
+## Test it out
 fortune ${INFILE}
